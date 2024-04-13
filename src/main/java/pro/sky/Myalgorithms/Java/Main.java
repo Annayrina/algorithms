8d418edcbcb87418d7fbd9b9d59e838eca6c048a
Anna <annayrina1998@mail.ru>
2024-04-13 14:32:37 +0700

    dz

---
+++ src/main/java/pro/sky/Myalgorithms/Java/Main.java
@@ -0,0 +1,9 @@
+package pro.sky.Myalgorithms.Java;
+
+import pro.sky.Myalgorithms.MyStringList;
+
+public class Main {
+    public static void main(String[] args) {
+        MyStringList myStringList = new MyStringList(10);
+    }
+}
